bcedf3c150d7bbc30903a555b2b270943a167ebf
Akubrecah <poweldayck@gmail.com>
2025-12-18 10:58:27 +0300

    Fix Mongoose connection options

--- server.js
+++ server.js
@@ -14,10 +14,7 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/'))); // Serve static files from root
 
 // Database Connection
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('MongoDB Connected'))
 .catch(err => console.log(err));
 
